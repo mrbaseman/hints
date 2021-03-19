6cb8f2038b4c09f1307ff88c20a05642e30f539a
Martin Hecht <mrbaseman@gmx.de>
2021-03-20 00:43:42 +0100

    moved to my own repository

--- modify.php
+++ modify.php
@@ -5,8 +5,8 @@
  * @package         Hints
  * @version         0.6.2
  * @authors         Martin Hecht (mrbaseman), Ruud Eisinga (Dev4me)
- * @copyright       (c) 2018 - 2020, Martin Hecht
- * @link            https://github.com/WebsiteBaker-modules/hints
+ * @copyright       (c) 2018 - 2021, Martin Hecht
+ * @link            https://github.com/mrbaseman/hints
  * @license         GNU General Public License v3 - The javascript features are third party software, spectrum color picker and autosize, both licensed under MIT license
  * @platform        2.8.x
  * @requirements    PHP 7.x
